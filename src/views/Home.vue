08a639ee37bc326dde5d27f95780980da42f7664
Jacob Speakman <speakman610@gmail.com>
2021-12-04 21:49:33 -0700

    home page icon links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,8 @@
     <h1>Welcome to the Custom Catalog</h1>
     <div id="entertainment">
       <router-link to="/books"><div class="ent-object"><img alt="Books" src="../assets/open-book.png"><h3>Read a book</h3></div></router-link>
-      <div class="ent-object"><img alt="Movies/TV" src="../assets/film-reel.png"><h3>Watch a movie</h3></div>
-      <div class="ent-object"><img alt="Gamers" src="../assets/board-game.png"><h3>Play a game</h3></div>
+      <router-link to="/movies"><div class="ent-object"><img alt="Movies/TV" src="../assets/film-reel.png"><h3>Watch a movie</h3></div></router-link>
+      <router-link to="/games"><div class="ent-object"><img alt="Games" src="../assets/board-game.png"><h3>Play a game</h3></div></router-link>
     </div>
   </div>
 </template>
@@ -28,6 +28,13 @@
     /* background-color: rgb(110, 87, 87); */
     padding: 20px;
     min-width: 200px;
+  }
+
+  .ent-object:hover {
+    border: 1px solid #faf5dc;
+    border-radius: 50px;
+    background-color: #faf5dc73;
+    color: #0d1116;
   }
 
   .ent-object img {
